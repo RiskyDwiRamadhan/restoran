9090c1890bbfaa8d54031767e992c1a8e66e6486
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-05-23 23:33:03 +0700

    Tugas Besar

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,159 @@
+
+        <!-- Left Sidebar -->
+        <div class="left main-sidebar">
+
+            <div class="sidebar-inner leftscroll">
+
+                <div id="sidebar-menu">
+
+                    <ul>
+
+                        <li class="submenu">
+                            <a href="users.html">
+                                <i class="fas fa-user"></i>
+                                <span> Users </span>
+                            </a>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="blog.html">
+                                <i class="fas fa-file-alt"></i>
+                                <span> Makanan </span>
+                            </a>
+                        </li>
+
+                        <li class="submenu">
+                            <a id="tables" href="#">
+                                <i class="fas fa-table"></i>
+                                <span> Tables </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="tables-basic.html">Basic Tables</a>
+                                </li>
+                                <li>
+                                    <a href="tables-datatable.html">Data Tables</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#">
+                                <i class="fas fa-laptop"></i>
+                                <span> User Interface </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="list-unstyled">
+                               
+                                <li>
+                                    <a href="ui-icons.html">Icons</a>
+                                </li>
+                               
+                                <li>
+                                    <a href="ui-icons.html">Icons</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#">
+                                <i class="fab fa-wpforms"></i>
+                                <span> Forms </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="forms-general.html">General Elements</a>
+                                </li>
+                                <li>
+                                    <a href="forms-select2.html">Select2</a>
+                                </li>
+                                <li>
+                                    <a href="forms-validation.html">Form Validation</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#">
+                                <i class="fas fa-file-image"></i>
+                                <span> Multimedia </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="media-fancybox.html">
+                                        <span class="label radius-circle bg-danger float-right">cool</span> Fancybox </a>
+                                </li>
+                                <li>
+                                    <a href="media-lightbox.html">Lightbox</a>
+                                </li>
+                                <li>
+                                    <a href="media-owl-carousel.html">Owl Carousel</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#">
+                                <i class="fas fa-star"></i>
+                                <span> Plugins </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="sweetalert.html">SweetAlert</a>
+                                </li>
+                                <li>
+                                    <a href="counter-up.html">Counter-Up</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="submenu">
+                            <a href="#">
+                                <span class="label radius-circle bg-primary float-right">9</span>
+                                <i class="fas fa-indent"></i>
+                                <span> Menu Levels </span>
+                            </a>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <span>Second Level</span>
+                                    </a>
+                                </li>
+                                <li class="submenu">
+                                    <a href="#">
+                                        <span>Third Level</span>
+                                        <span class="menu-arrow"></span>
+                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">
+                                                <span>Third Level Item</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <span>Third Level Item</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+
+                        </li>
+
+                    </ul>
+
+                    <div class="clearfix"></div>
+
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
+
+        </div>
+        <!-- End Sidebar -->
