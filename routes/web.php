b2c7d36ffbecbe5f9977f915fd0aec43561904cd
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-05-29 13:57:12 +0700

    Tugas Besar

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\DetailOrderController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\MejaController;
-use App\Http\Controllers\UserDataController;
+use App\Http\Controllers\TransaksiController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -24,17 +24,27 @@ use Illuminate\Support\Facades\Auth;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
+    // // Route::get('/home', [HomeController::class, 'home'])->name('home.home');
+    // Route::get('/menus', [HomeController::class, 'menu'])->name('home.menu');
+    // Route::get('/meja', [HomeController::class, 'meja'])->name('home.meja');
+    // // Route::get('/admin', [HomeController::class, 'admin'])->name('home.admin');
 
-// Route::get('/', [HomeController::class, 'index'])->name('home.index');
-// Route::get('/menus', [HomeController::class, 'menu'])->name('home.menu');
-// Route::get('/meja', [HomeController::class, 'meja'])->name('home.meja');
-// Route::get('/admin', [HomeController::class, 'admin'])->name('home.admin');
+    // Route::get('/sementara/{id}', [DetailOrderController::class, 'simpanSementara'])->name('detailorder.sementara');
+    // Route::get('/save', [DetailOrderController::class, 'save'])->name('detailorder.save');
+    // Route::get('/bayar/{id}', [TransaksiController::class, 'transaksi'])->name('transaksi.bayar');
+    // Route::get('/proses/{id}', [TransaksiController::class, 'save'])->name('transaksi.proses');
+    // Route::get('/transaksi/admin', [TransaksiController::class, 'admin'])->name('transaksi.admin');
 
-// Route::get('/sementara/{id}', [DetailOrderController::class, 'simpanSementara'])->name('detailorder.sementara');
-// Route::get('/save', [DetailOrderController::class, 'save'])->name('detailorder.save');
+    // Route::resource('order', OrderController::class);
+    // Route::resource('detailorder', DetailOrderController::class);
+    // Route::resource('menu', MenuController::class);
+    // Route::resource('favorite', FavoriteController::class);
+    // // Route::resource('detailorder', DetailOrderController::class);
+    // Route::resource('booking', MejaController::class);
+    // Route::resource('transaksi', TransaksiController::class);
 
-// Route::resource('order', OrderController::class);
-// Route::resource('detailorder', DetailOrderController::class);
+Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
  
@@ -56,7 +66,21 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/home', [HomeController::class, 'home'])->name('home.home');
         Route::get('/menus', [HomeController::class, 'menu'])->name('home.menu');
         Route::get('/meja', [HomeController::class, 'meja'])->name('home.meja');
-    });
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Route::resource('userdata', UserDataController::class);
+        Route::resource('detailorder', DetailOrderController::class);
+        Route::get('/sementara/{id}', [DetailOrderController::class, 'simpanSementara'])->name('detailorder.sementara');
+        Route::get('/save', [DetailOrderController::class, 'save'])->name('detailorder.save');
+        
+        Route::resource('transaksi', TransaksiController::class);
+        Route::get('/bayar/{id}', [TransaksiController::class, 'transaksi'])->name('transaksi.bayar');
+        Route::get('/proses/{id}', [TransaksiController::class, 'save'])->name('transaksi.proses');
+        Route::get('/transaksi/admin', [TransaksiController::class, 'admin'])->name('transaksi.admin');
+    });
+ 
+    Route::get('/logout', function() {
+        Auth::logout();
+        redirect('/home');
+    });
+ 
+});
+ 
